06d728847453cf0f3ff82bf2572d1fa8906667f3
IvanArjonaAmadorUPM <ivan.arjona.amador@alumnos.upm.es>
2021-12-01 17:47:43 +0100

    creación corrutinas y peticiones a la API

--- app/src/main/java/com/example/crud_app/MainActivity.kt
+++ app/src/main/java/com/example/crud_app/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.crud_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.crud_app.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +21,26 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    }   
+    }
+    /*Función que obtiene la url de la API externa
+    y convierte los datos de JSON al modelo creado*/
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder().baseUrl("https://hello-world.innocv.com/api/User/").
+        addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    /*creación de corrutinas para evitar que la aplicacion
+    se bloquee al realizar peticiones al servidor*/
+    private fun searchById(query:Int){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call : Response<UserResponse> = getRetrofit().create(APIService::class.java).getUserById("$query")
+            val user: UserResponse? = call.body()
+            if(call.isSuccessful){
+            //show recycled
+            }
+            else{
+                //show error
+            }
+        }
+    }
 }
